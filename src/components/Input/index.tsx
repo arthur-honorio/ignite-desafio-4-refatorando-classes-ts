39b8304156682c0b8e513d38e4d7dcb7c7c8faac
Arthur Honorio <hno.arthur@gmail.com>
2022-01-26 23:26:58 -0300

    updating header input and modal

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,14 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string
+  placeholder: string
+}
+
+const Input = ({ name, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  console.log(inputRef.current)
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -24,7 +30,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(inputRef.current?.value ? true : false);
   }, []);
 
   useEffect(() => {
@@ -37,16 +43,15 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
 
-      <input
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        defaultValue={defaultValue}
-        ref={inputRef}
-        {...rest}
-      />
-    </Container>
+    <input
+      onFocus={handleInputFocus}
+      onBlur={handleInputBlur}
+      defaultValue={defaultValue}
+      ref={inputRef}
+      {...rest}
+    />
+  </Container>
   );
 };
 
